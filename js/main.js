05d5facb3bacadacc781c2ae4ffe005e68b4a56c
Curso <alberto.ie2cmfp@gmail.com>
2017-05-11 15:12:33 +0200

    Validacion de formulario de contacto.

--- js/main.js
+++ js/main.js
@@ -7,28 +7,72 @@ jQuery(document).ready(function($) {
     function validarFormularioContacto(){
         //recoger los valores de la vista
         var pdni = $("#dni").val();
+        var pnombre = $("#nombre").val();
+        var papellidos = $("#apellidos").val();
+        var ptelefono = $("#telefono").val();
+        var valido = false;
         //evaluarlos
-        var enviado= validarDni(pdni); //en funcion de si estan bien o mal o se envia o no
-        if(enviado){
+        var dniValido= validarDni(pdni); //en funcion de si estan bien o mal o se envia o no
+        var nomValido = validarNombre(pnombre);
+        var apeValido = validarApellidos(papellidos);
+        var teleValido = validarTelefono(ptelefono);
+        if(dniValido&&nomValido&&apeValido&&teleValido){
             // $("#contactForm").submit();//se envia el Formulario(Consumir REST)
+            valido = true;
+            $("#dni").siblings("div.text-error").text("");
+            $("#nombre").siblings("div.text-error").text("");
+            $("#apellidos").siblings("div.text-error").text("");
+            $("#telefono").siblings("div.text-error").text("El telefono no es valido, tiene que tener 9 numeros");
+
         }else {
             //mostar mensaje de error
-            $("#dni").siblings("div.text-error").text("El DNI esta mal formado");
+            if(!dniValido){
+                $("#dni").siblings("div.text-error").text("El DNI esta mal formado");
+            }
+            if(!nomValido){
+                $("#nombre").siblings("div.text-error").text("El nombre tiene que tener al menos 3 letras");
+            }
+            if(!apeValido){
+                $("#apellidos").siblings("div.text-error").text("Los apellidos tienen que tener al menos 7 letras");
+            }
+            if(!teleValido){
+                $("#telefono").siblings("div.text-error").text("El telefono no es valido, tiene que tener 9 numeros");
+            }
             //text y html
         }
-        return enviado;
+        return valido;
     }
 });
 
+function validarNombre(nombre){
+    const pattern = new RegExp("[a-zA-Z]{3,}");
+    return pattern.test(nombre);
+}
+function validarApellidos(apellidos) {
+    const pattern = new RegExp("[a-zA-Z]{2,}\s[a-zA-Z]{2,}");
+    return pattern.test(apellidos);
+}
+function validarTelefono(telefono){
+    var valido = true;
+    if(telefono!=""){
+        const pattern = new RegExp("\d{9}");
+        valido = pattern.test(telefono);
+    }
+    return valido ;
+}
+
 function validarDni(dni) {
-    var valido =true;
-    numero = parseInt(dni.substr(0,dni.length-1),10);
-    letr = dni.substr(dni.length-1,1);
-    numero = numero % 23;
-    letra='TRWAGMYFPDXBNJZSQVHLCKET';
-    letra=letra.substring(numero,numero+1);
-    if (letra!=letr.toUpperCase()) {
-        valido = false;
+    var valido =false;
+    const pattern = new RegExp("\d{8}[a-zA-Z]");
+    if(pattern.test(dni)){
+        numero = parseInt(dni.substr(0,dni.length-1),10);
+        letr = dni.substr(dni.length-1,1);
+        numero = numero % 23;
+        letra='TRWAGMYFPDXBNJZSQVHLCKET';
+        letra=letra.substring(numero,numero+1);
+        if (letra==letr.toUpperCase()) {
+            valido = true;
+        }
     }
     return valido;
 }
